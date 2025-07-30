4f0e812bb0ff86456444a18b0dee035b49b105ef
martinm07 <martin.github07@gmail.com>
2025-07-30 18:04:50 +0100

    Change to ZSH shell Enable programs.nix-ld for future DLLs like in Zed extensions. Add research comment about using `zed-editor` vs. `zed-editor-fsh`.

--- dm01.nix
+++ dm01.nix
@@ -155,7 +155,9 @@ in {
     packages = with pkgs; [
       thunderbird
     ];
+    shell = pkgs.zsh;
   };
+  programs.zsh.enable = true;
 
   # Enable automatic login for the user.
   services.displayManager.autoLogin.enable = true;
@@ -180,6 +182,20 @@ in {
     # --- DEVELOPER TOOLS ---
     # --- --- --- --- --- ---
     kitty
+    # There's also `zed-editor-fhs` for wrapping the editor in a Filesystem Hierarchy Standard (FHS) sandbox, allegedly
+    #  for allowing extensions to work without Nix-specific configuration. These are extensions that try to run
+    #  dynamically linked libraries (DLLs) e.g. that are language servers (like rust-analyzer): https://github.com/NixOS/nixpkgs/issues/309662
+    # Refer to this for the use-cases of an FHS wrapper: https://nixos.org/manual/nixpkgs/stable/#sec-fhs-environments
+    # However, this does create inconvenciences, like the Zed terminal running isolated from the regular system, missing everything not
+    #  in the user directory like /etc, /nix/store, etc. and missing certain privelages. As well as some funky stuff potentially freezing the system
+    #  (TODO: waiting to see if that happens again after switching off of zed-editor-fhs)
+    #  and not getting PATH updates after system version switches (just speculation, though).
+    # I'm pretty sure the problem with DLLs can also be fixed by using programs.nix-ld. Try to use that before switching back to zed-editor-fhs.
+    #
+    # From what I read there also seems to be another issue with `zed-editor` (not -fhs) where during installing it tries to install some default pre-built LSPs
+    #  into ~/.local/share/zed/languages/ (especially a JSON LSP for editing Zed's settings.json) and fails to do that because of Nix.
+    # Since I started with zed-editor-fhs I got these files in my home directory, and after that the regular `zed-editor` was fine (i.e. I got lucky :))
+    # See:   https://github.com/NixOS/nixpkgs/issues/421750     and:   https://wiki.nixos.org/wiki/Zed#LSP_Support
     zed-editor
     nixd # Nix LSP
     alejandra # Nix formatter
@@ -236,6 +252,12 @@ in {
     user.name = "martinm07";
     user.email = "martin.github07@gmail.com";
   };
+
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = [
+    # Includes packages that imperative DLLs (for example, downloaded by Zed extensions) are asking to have exist
+    #  in the library path.
+  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
