a61568b59745f36afc277d9ed3f3e7317f5b6114
martinm07 <martin.github07@gmail.com>
2025-08-06 13:52:07 +0100

    Experiment with enabling avahi instead

--- dm01.nix
+++ dm01.nix
@@ -172,14 +172,14 @@ in {
   hardware.sane.extraBackends = [pkgs.sane-airscan];
 
   # For scanner discovery by other programs; udev assigns "predictable names" to network interfaces
-  services.udev.packages = [pkgs.sane-airscan];
+  # services.udev.packages = [pkgs.sane-airscan];
 
   # Allow printer discovery on local network
-  # services.avahi = {
-  #   enable = true;
-  #   nssmdns4 = true;
-  #   openFirewall = true;
-  # };
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
 
   hardware.printers = {
     ensurePrinters = [
