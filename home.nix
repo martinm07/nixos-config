280e33be53067fe714650d9cde1e7279be9546b9
martinm07 <martin.github07@gmail.com>
2025-12-07 19:45:51 +0000

    Test removing linkDotFiles script

--- home.nix
+++ home.nix
@@ -8,29 +8,29 @@
   home.homeDirectory = "/home/martinm";
   home.stateVersion = "25.05";
 
-  home.activation = {
-    linkDotfiles = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      #!/usr/bin/env bash
+  # home.activation = {
+  #   linkDotfiles = lib.hm.dag.entryAfter ["writeBoundary"] ''
+  #     #!/usr/bin/env bash
 
-      link_config() {
-        local target=$1
-        local link=$2
+  #     link_config() {
+  #       local target=$1
+  #       local link=$2
 
-        mkdir -p "$(dirname "$link")"
+  #       mkdir -p "$(dirname "$link")"
 
-        if [ -e "$link" ] && [ ! -L "$link" ]; then
-          $DRY_RUN_CMD echo "Warning: $link exists and is not a symlink, skipping"
-        else
-          $DRY_RUN_CMD rm -f "$link"
-          $DRY_RUN_CMD ln -sf "$target" "$link"
-        fi
-      }
+  #       if [ -e "$link" ] && [ ! -L "$link" ]; then
+  #         $DRY_RUN_CMD echo "Warning: $link exists and is not a symlink, skipping"
+  #       else
+  #         $DRY_RUN_CMD rm -f "$link"
+  #         $DRY_RUN_CMD ln -sf "$target" "$link"
+  #       fi
+  #     }
 
-      # -------------------------------------------------------------------------
-      #  MAKE SYMLINKS FROM DOTFILE LOCATIONS TO FILE LOCATIONS IN THIS GIT REPO
-      # -------------------------------------------------------------------------
-      link_config ~/.config/system/zshrc ~/.zshrc
-      # link_config ~/.config/system/ironbar ~/.config/ironbar
-    '';
-  };
+  #     # -------------------------------------------------------------------------
+  #     #  MAKE SYMLINKS FROM DOTFILE LOCATIONS TO FILE LOCATIONS IN THIS GIT REPO
+  #     # -------------------------------------------------------------------------
+  #     link_config ~/.config/system/zshrc ~/.zshrc
+  #     # link_config ~/.config/system/ironbar ~/.config/ironbar
+  #   '';
+  # };
 }
