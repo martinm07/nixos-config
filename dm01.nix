959d3ee672046428d6fa8b9a3066d66032ca2a2d
martinm07 <martin.github07@gmail.com>
2025-12-09 18:37:46 +0000

    Enable fcitx5 waylandFrontend

--- dm01.nix
+++ dm01.nix
@@ -143,6 +143,7 @@ in {
     # ibus.engines = with pkgs.ibus-engines; [mozc];
     type = "fcitx5";
     fcitx5.addons = with pkgs; [fcitx5-mozc fcitx5-gtk];
+    fcitx5.waylandFrontend = true;
   };
 
   environment.variables = {
