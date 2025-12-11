3d03bb9ed0e37a0b18001bdfbd4d14ffcf120d9c
martinm07 <martin.github07@gmail.com>
2025-12-11 15:51:22 +0000

    Update default Noto fonts names

--- dm01.nix
+++ dm01.nix
@@ -185,7 +185,8 @@ in {
     enableDefaultPackages = true;
     packages = with pkgs; [
       noto-fonts-cjk-sans
-      noto-fonts-cjk-serif-static
+      noto-fonts-cjk-serif
+      noto-fonts
       ubuntu-classic
       fira-code
       nerd-fonts.jetbrains-mono
@@ -193,9 +194,22 @@ in {
 
     fontconfig = {
       defaultFonts = {
-        sansSerif = ["Noto Sans CJK"];
-        serif = ["Noto Serif CJK"];
-        # serif = ["Ubuntu Mono"];
+        sansSerif = [
+          "Noto Sans CJK JP"
+          "Noto Sans CJK SC"
+          "Noto Sans CJK TC"
+          "Noto Sans CJK HK"
+          "Noto Sans CJK KR"
+          "Noto Sans"
+        ];
+        serif = [
+          "Noto Serif CJK JP"
+          "Noto Serif CJK SC"
+          "Noto Serif CJK TC"
+          "Noto Serif CJK HK"
+          "Noto Serif CJK KR"
+          "Noto Serif"
+        ];
         monospace = ["Ubuntu Mono"];
       };
     };
