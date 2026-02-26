c5c8e8071bc42c377b203966202eb891af843bfd
martinm07 <martin.github07@gmail.com>
2026-02-26 15:04:35 +0000

    Switch Tauon to unstable branch and enable persistent whiteboard and blackboard for wayscriber

--- dm01.nix
+++ dm01.nix
@@ -554,7 +554,7 @@ in {
     gimp # GNU Image Manipulation Program
     zoom-us
     fatsort # Main use-case is sorting MP3 files on USB flash drives for CD players
-    tauon # Music player
+    nixpkgsUnstable.legacyPackages.x86_64-linux.tauon # Music player
     puddletag # For adding metadata ("tags") to MP3/audio files (like title, album, cover art, etc.), supporting automatic patterns from the filenames
     nixpkgsUnstable.legacyPackages.x86_64-linux.ollama-rocm
   ];
