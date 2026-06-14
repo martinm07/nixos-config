93d8e63ad76536beb044ce95dc9ba012c9864230
martinm07 <martin.github07@gmail.com>
2026-06-14 14:01:41 +0100

    Enable natural scrolling on touchpad

--- hosts/m02/configuration.nix
+++ hosts/m02/configuration.nix
@@ -57,6 +57,9 @@
     shell = pkgs.zsh;
   };
 
+  # Makes it so that swiping down scrolls up
+  services.libinput.touchpad.naturalScrolling = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
