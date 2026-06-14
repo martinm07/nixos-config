f57c56725f3a5d33e66d4209064e5c9db4aa630b
martinm07 <martin.github07@gmail.com>
2026-06-14 14:15:37 +0100

    Disable natural scrolling

--- hosts/m02/configuration.nix
+++ hosts/m02/configuration.nix
@@ -58,7 +58,7 @@
   };
 
   # Makes it so that swiping down scrolls up
-  services.libinput.touchpad.naturalScrolling = true;
+  services.libinput.touchpad.naturalScrolling = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
